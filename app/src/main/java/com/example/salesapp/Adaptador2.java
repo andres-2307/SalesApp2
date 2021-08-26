6db12fbdfb3e75f26d3a013c5c392c70a8bc4249
andres-2307 <58479384+andres-2307@users.noreply.github.com>
2021-08-25 19:57:27 -0500

    venta

--- app/src/main/java/com/example/salesapp/Adaptador2.java
+++ app/src/main/java/com/example/salesapp/Adaptador2.java
@@ -11,27 +11,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class Adadtador extends RecyclerView.Adapter<Adadtador.ViewHolder> implements View.OnClickListener {
+public class Adaptador2 extends RecyclerView.Adapter<Adadtador.ViewHolder> implements View.OnClickListener {
 
 
     ArrayList<Producto> datos;
     private View.OnClickListener listener;
 
-    public Adadtador(ArrayList<Producto> dato) {
+    public Adaptador2(ArrayList<Producto> dato) {
         this.datos = dato;
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public Adadtador.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.formato_lista,null,false);
         view.setOnClickListener(this);
-        return new ViewHolder(view);
+        return new Adadtador.ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull Adadtador.ViewHolder holder, int position) {
 
         holder.nombre.setText(datos.get(position).getNombre());
         holder.id.setText(datos.get(position).getId());
@@ -46,21 +46,21 @@ public class Adadtador extends RecyclerView.Adapter<Adadtador.ViewHolder> implem
         return datos.size();
     }
 
-public void setOnClickListener(View.OnClickListener listener){
-this.listener=listener;
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener=listener;
 
-}
+    }
 
     @Override
     public void onClick(View v) {
 
 
-           listener.onClick(v);
+        listener.onClick(v);
 
 
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView nombre,id,precio,tipo;
         ImageView foto;
--- app/src/main/java/com/example/salesapp/Productos.java
+++ app/src/main/java/com/example/salesapp/Productos.java
@@ -16,8 +16,13 @@ import java.util.ArrayList;
 
 public class Productos extends AppCompatActivity {
     ArrayList<Producto> datos;
-    RecyclerView recycler;
+    ArrayList<Producto> mostrar;
+    ArrayList<Producto> venta;
+    RecyclerView recycler,recycler2;
+    private float precio;
+
     private Button but_buscar;
+    private  TextView costo;
 
     private EditText buscar;
 
@@ -28,12 +33,22 @@ public class Productos extends AppCompatActivity {
 
         but_buscar =(Button)findViewById(R.id.but_buscar);
         buscar=(EditText) findViewById(R.id.edit_buscar);
+        costo=(TextView) findViewById(R.id.total);
+
+
 
 
         recycler=(RecyclerView) findViewById(R.id.idrecycler);
         recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
 
+
+        recycler2=(RecyclerView) findViewById(R.id.idrecycler2);
+        recycler2.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+
         datos= new ArrayList<>();
+
+
+
 
         datos.add(0, new Producto("azucar", "$ 5.000", "ID 1003","de caña", R.drawable.ic_azucar));
         datos.add(1, new Producto("azucar", "$ 7.000", "ID 1004","de coco", R.drawable.ic_azucar));
@@ -44,20 +59,44 @@ public class Productos extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
-                ArrayList<Producto> mostrar;
                 mostrar = new ArrayList<>();
+                venta = new ArrayList<>();
 
                     int i;
                     for (i=0;i<datos.size();) {
                         Producto dat = datos.get(i);
 
+
                         if (dat.getNombre().equals(buscar.getText().toString())) {
 
                             mostrar.add(new Producto(dat.getNombre(), dat.getPrecio(), dat.getId(),dat.getTipo(), dat.getFoto()));
 
-                            Adadtador adata = new Adadtador(mostrar);
-                            recycler.setAdapter(adata);
+                            Adadtador adapta = new Adadtador(mostrar);
+                            recycler.setAdapter(adapta);
+
+                            adapta.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+
+
+                                    venta.add(new Producto(mostrar.get(recycler.getChildAdapterPosition(v)).getNombre(),
+                                                             mostrar.get(recycler.getChildAdapterPosition(v)).getPrecio(),
+                                                             mostrar.get(recycler.getChildAdapterPosition(v)).getId(),
+                                                             mostrar.get(recycler.getChildAdapterPosition(v)).getTipo(),
+                                                             mostrar.get(recycler.getChildAdapterPosition(v)).getFoto()));
+
+
+                                    Adaptador2 adapta2 = new Adaptador2(venta);
+                                    recycler2.setAdapter(adapta2);
+
+
+                                  ;
+
+                                  costo.setText(mostrar.get(recycler.getChildAdapterPosition(v)).getPrecio());
+
+                                }
+                            });
 
                         } else {
                             Toast.makeText(Productos.this, buscar.getText()+" NO SE ENCONTRO", Toast.LENGTH_SHORT).show();
