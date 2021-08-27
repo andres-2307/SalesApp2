c80904563b775e8e8ccd0130f60862f009e4ffdf
andres-2307 <58479384+andres-2307@users.noreply.github.com>
2021-08-27 12:03:45 -0500

    cliente

--- app/src/main/java/com/example/salesapp/Inicio.java
+++ app/src/main/java/com/example/salesapp/Inicio.java
@@ -10,13 +10,15 @@ import android.widget.TextView;
 public class Inicio extends AppCompatActivity {
 
     private TextView producto;
-
+    private TextView cliente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-        producto=findViewById(R.id.id_producto);
+        producto=findViewById(R.id.id_venta);
+
+        cliente=findViewById(R.id.id_cliente);
 
 
 producto.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +29,13 @@ producto.setOnClickListener(new View.OnClickListener() {
 
 });
 
+        cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Inicio.this,Cliente.class));
+            }
+
+        });
 
 
 
